3573f99611fcdf48c05baad9507ef9a9005570bd
gulfa <gulfa@DESKTOP-QKVA7TJ>
2026-01-11 11:43:34 +0530

    xxx2

--- src/test/java/com/gul/web2/DownloadImageURL.java
+++ src/test/java/com/gul/web2/DownloadImageURL.java
@@ -7,7 +7,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
-import java.time.Instant;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -16,7 +15,6 @@ import org.springframework.util.ResourceUtils;
 public class DownloadImageURL {
 
 	public static void main(String[] args) throws Exception {
-	
 
 //		        
 
@@ -26,9 +24,10 @@ public class DownloadImageURL {
 		System.out.println("processing excel file " + f.getName());
 		List<WebData> records = ExcelReader.parseExcelFileToBeans(stream, f);
 		for (WebData excel : records) {
-			
+
 			// TODO Auto-generated method stub
-			long millis = Instant.now().toEpochMilli();
+//			long millis = Instant.now().toEpochMilli();
+			long millis = System.currentTimeMillis();
 			System.out.println(millis);
 			String imageUrl = excel.getUserImageLink();
 //			String imageUrl = "https://cdni.pornpics.de/1280/7/827/16936545/16936545_018_1042.jpg";
@@ -36,10 +35,12 @@ public class DownloadImageURL {
 
 			HttpRequest request = HttpRequest.newBuilder().uri(URI.create(imageUrl)).build();
 
-			client.send(request, HttpResponse.BodyHandlers.ofFile(Path.of("E:/GulDoc/xxximg/" + millis + ".jpg")));
-
+			client.send(request, HttpResponse.BodyHandlers
+					.ofFile(Path.of("E:/GulDoc/xxximg/" + millis + ".jpg")));
+//			Thread.sleep(1000);
 			System.out.println("Image downloaded!");
 		}
+		System.out.println("Done");
 	}
 
 }
